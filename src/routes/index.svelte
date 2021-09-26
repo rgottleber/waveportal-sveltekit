f3fcf38c36ecd715c331884188ace07134c554cf
Richard Gottleber <richard@gottleber.net>
2021-09-26 13:48:41 -0500

    Add metamask connect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,34 @@
 <script>
 	import { onMount } from 'svelte';
 	import { ethers } from 'ethers';
+	$: account = null;
 	onMount(async () => {
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		try {
+			account = await signer.getAddress();
+		} catch (error) {}
 	});
 	async function attachWallet() {
-		await window.ethereum.enable();
+		const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+		// Prompt user for account connections
+		await provider.send('eth_requestAccounts', []);
+		const signer = provider.getSigner();
+		account = await signer.getAddress();
 	}
 </script>
 
-<h1>Howdy</h1>
-<button on:click={attachWallet}>Attach Wallet</button>
+<h1>Howdy 👋</h1>
+<p>I'm Richard, this is a fun little place to wave at me on the blockchain</p>
+
+{#if !account}
+	<button on:click={attachWallet}>Attach Wallet</button>
+{:else}
+	<p>You're connected to the wallet!</p>
+{/if}
+
+<style>
+	h1 {
+		text-align: center;
+	}
+</style>
